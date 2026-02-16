ec3c9d9c3533120af79d56db52c7364f6d8c631d
Daniel Shuler <dshuler92@gmail.com>
2026-02-15 16:03:14 -0800

    practice: letter grade function with user input

---
+++ practice.py
@@ -0,0 +1,15 @@
+def letter_grade(grade):
+    if grade <= 100 and grade >= 90:
+        print("A")
+    elif grade >= 80 and grade < 90:
+        print("B")
+    elif grade >= 70 and grade <80:
+        print("C")
+    elif grade >= 60 and grade < 70:
+        print("D")
+    else:
+        print("F")
+
+user_input = int(input("Enter a numeric grade: "))
+
+letter_grade(user_input)
